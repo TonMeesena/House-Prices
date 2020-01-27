32f263d91ede82558426526648da4f223b614d9a
wasinmeesena <wasinmeesena@gmail.com>
2020-01-26 21:42:55 -0600

    linear regression

--- Rcode.R
+++ Rcode.R
@@ -6,5 +6,11 @@ library("ggplot2")
 names(train)
 str(train)
 
-ggplot(train,aes(x=LotArea,y=SalePrice))+geom_point()+geom_smooth()
+ggplot(train,aes(x=SalePrice,y=LotArea))+geom_point()
 cor(train$SalePrice, train$LotArea, method = c("pearson", "kendall", "spearman"))
+
+lm1<-lm(SalePrice~ poly(LotArea,2),data=train)
+summary(lm1)
+plot(train$LotArea,train$SalePrice)
+lines(sort(train$LotArea), fitted(lm1)[order(train$LotArea)], col='red', type='b') 
+
