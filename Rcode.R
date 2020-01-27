87eba19b606b84c08a475f2e9d3a25ee639257e9
wasinmeesena <wasinmeesena@gmail.com>
2020-01-26 21:09:09 -0600

    wow

--- Rcode.R
+++ Rcode.R
@@ -1,2 +1,10 @@
-train<-read.csv("train.csv")
+train<-read.csv("train.csv",header = TRUE)
 test<-read.csv("test.csv")
+Sale<-train$SalePrice
+
+library("ggplot2")
+names(train)
+str(train)
+
+ggplot(train,aes(x=LotArea,y=SalePrice))+geom_point()+geom_smooth()
+cor(train$SalePrice, train$LotArea, method = c("pearson", "kendall", "spearman"))
