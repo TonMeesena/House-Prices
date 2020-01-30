ef59a3206f55dd1ede68e9a73f08f3642b229ef5
Lab User <labuser@libr451-04r.its.carleton.edu>
2020-01-30 15:30:20 -0600

    sdv

--- Rcode.R
+++ Rcode.R
@@ -9,8 +9,9 @@ str(train)
 ggplot(train,aes(x=SalePrice,y=LotArea))+geom_point()
 cor(train$SalePrice, train$LotArea, method = c("pearson", "kendall", "spearman"))
 
-lm1<-lm(SalePrice~ poly(LotArea,2),data=train)
+lm1<-lm(SalePrice~ poly(LotArea,1),data=train)
 summary(lm1)
 plot(train$LotArea,train$SalePrice)
 lines(sort(train$LotArea), fitted(lm1)[order(train$LotArea)], col='red', type='b') 
 
+summary(train)
