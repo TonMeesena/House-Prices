d044f1bb226bd990bf07b1a56edfee77cfe48b7e
wasinmeesena <wasinmeesena@gmail.com>
2020-02-10 01:50:16 -0600

    qqq

--- Rcode.R
+++ Rcode.R
@@ -320,3 +320,17 @@ ggplot(data=train,aes(x=OverallQual,y=SalePrice,color=Utilities))+geom_point()
 #LotConfig
 ggplot(data=train,aes(x=OverallQual,y=SalePrice,color=LotConfig))+geom_point()
 ggplot(data=train,aes(x=OverallQual,y=SalePrice,fill=LotConfig))+geom_bar(stat="summary",fun.y="mean")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
